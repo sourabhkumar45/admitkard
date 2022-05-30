9f017936b9681b66d46498e30ac7b474d65e0df4
sourabhkumar45 <sourabhkumar766709@gmail.com>
2022-05-30 12:44:40 +0530

    done

--- server/index.js
+++ server/index.js
@@ -25,6 +25,15 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.header("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, OPTIONS");
+  next();
+});
 app.use("/add", addRouter);
 app.use("/search", searchRouter);
 app.listen(PORT, (err) => {
