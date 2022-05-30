3213e3aa295068ec685abae1a9c07762356fbffe
sourabhkumar45 <sourabhkumar766709@gmail.com>
2022-05-30 13:25:39 +0530

    doen

--- server/index.js
+++ server/index.js
@@ -26,13 +26,13 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Credentials", true);
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
   );
   res.header("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, OPTIONS");
-  next();
 });
 app.use("/api/add", addRouter);
 app.use("/api/search", searchRouter);
